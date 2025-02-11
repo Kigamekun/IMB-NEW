072279bc20764380b3c95ad64e0da2376106ef9f
fawwazsaputra_ <manusiacoding29@gmail.com>
2025-02-11 13:49:41 +0700

    try to fix preview

--- app/Http/Controllers/SuratController.php
+++ app/Http/Controllers/SuratController.php
@@ -861,7 +861,7 @@ class SuratController extends Controller
 
         $details = json_encode($request->input('details'));
         $details2 = json_encode($request->input('details2'));
-        dd($details, $details2);
+        // dd($details, $details2);
         // dd($details);
 
         // Untuk Kabupaten
